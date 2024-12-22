59c86d70654034c2642e08484727a8b4cf70fd7c
Emre Özcan <emre@emreis.com>
2024-12-22 20:41:19 +0300

    Set a more suitable line height for the used fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import Title from "$lib/components/Title.svelte";
+</script>
 
-homepage.
+<Title title="Homepage" />
+<p>This is the home page.</p>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo cum, harum nihil sapiente magnam accusantium aspernatur tempore aut dolore ducimus exercitationem, beatae similique dolor obcaecati ipsum provident expedita! Architecto, voluptatibus.
+</p>
